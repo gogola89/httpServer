3b6ac65da6f8ff1afa47d88ed45474825e5d9cb4
George Ogola <gogola89@gmail.com>
2022-06-21 12:55:21 +0300

    initial commit

---
+++ src/app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const morgan = require("morgan");
+
+const app = express();
+
+app.use(cors());
+
+app.use(morgan("combined"));
+
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "..", "public")));
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+module.exports = app;
